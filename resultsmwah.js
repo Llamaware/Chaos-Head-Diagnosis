f5d7900943d3c80cd70c64e274092c13c503040b
OneHalf <49249543+KawaiiFiveO@users.noreply.github.com>
2023-12-20 12:55:08 -0800

    Update resultsmwah.js

--- resultsmwah.js
+++ resultsmwah.js
@@ -13,26 +13,18 @@ function clearBoxes(){
 //Calculates your score
 function calculateScore() {
     var totalChecked = document.querySelectorAll('input[type="checkbox"]:checked') 
-    var score = 130 - totalChecked.length 
+    var score = totalChecked.length 
 
     //Associates your score with what it says about you
     var meaning;
-    if (score >= 130) {
-        meaning = "You do not read fanfiction."
-    } else if (score >= 120) {
-        meaning = "You&#39ve read like. one fanfic and probably against your will. Or you are incredibly pure."
-    } else if (score >= 100) {
-        meaning = "I feel like you&#39re more into fluff and coffee shop AUs"
-     } else if (score >= 78) {
-        meaning = "You&#39ve seen your fair share but probably didn&#39t try to explore <em>that</em> much. you probably just stick to bdsm at most"
-       } else if (score >= 50) {
-        meaning = "There&#39s something a little bit off about you. It was the A/B/O wasn&#39t it."    
-    } else if (score >= 35) {
-        meaning = "God gave you the midnight hours to explore the deep blue sea of AO3"
-    } else if (score >= 11) {
-        meaning = "Virgin in theory, whore in practice."
-    } else {
-        meaning = "God is watching you" 
+    if (score >= 22) {
+        meaning = "You are completely and utterly delusional."
+    } else if (score >= 17) {
+        meaning = "You are extermely delusional."
+    } else if (score >= 7) {
+        meaning = "You are moderately delusional, which is the most common diagnosis."
+     } else {
+        meaning = "You are not delusional in the slightest." 
     }
 
     var scoreDiv = document.getElementById("score"); 
